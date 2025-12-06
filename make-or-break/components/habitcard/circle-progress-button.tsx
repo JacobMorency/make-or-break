c014919f1ddffac094947512992045dd2531f9d7
jacobmorency <jvmorency@gmail.com>
2025-12-05 22:53:27 -0500

    Update added temp circle button

--- make-or-break/components/habitcard/circle-progress-button.tsx
+++ make-or-break/components/habitcard/circle-progress-button.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
+import { useState } from "react";
 
 export default function CircleProgressButton() {
+  const [timesPressed, setTimesPressed] = useState(0);
   return (
-    <View className="bg-bg rounded-full w-12 h-12 items-center justify-center">
-      <Text className="text-text">0</Text>
+    <View>
+      <Pressable
+        className="bg-bg rounded-full w-12 h-12 items-center justify-center"
+        onPress={() => {
+          setTimesPressed((current) => current + 1);
+        }}
+      >
+        <Text className="text-text">{timesPressed}</Text>
+      </Pressable>
     </View>
   );
 }
--- make-or-break/components/habitcard/habitcard.tsx
+++ make-or-break/components/habitcard/habitcard.tsx
@@ -1,18 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, Modal } from "react-native";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import CircleProgressButton from "@/components/habitcard/circle-progress-button";
 
 export default function HabitCard() {
   return (
-    <View className="bg-card flex-row p-2 rounded-xl items-center justify-between my-1">
-      <View className="flex-row items-center gap-3">
-        <FontAwesome5 name="question-circle" size={24} color="text" />
-        <View>
-          <Text className="text-text">Habit Name</Text>
-          <Text className="text-text text-xs">Goal: 0</Text>
+    <View>
+      <Pressable className="bg-card flex-row p-2 rounded-xl items-center justify-between my-1">
+        <View className="flex-row items-center gap-3">
+          <FontAwesome5 name="question-circle" size={24} color="text" />
+          <View>
+            <Text className="text-text">Habit Name</Text>
+            <Text className="text-text text-xs">Goal: 0</Text>
+          </View>
         </View>
-      </View>
-      <CircleProgressButton />
+        <CircleProgressButton />
+      </Pressable>
     </View>
   );
 }
